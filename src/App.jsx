849742dcd3963c4e442fe75b2259c97a57409947
이재현 <2023050@bssm.hs.kr>
2024-06-03 00:24:14 +0900

    FIX :: App.jsx 제거

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,6 @@ function App() {
   return (
       <div className="App">
           <Sidebar />
-          <section>
-            <BettingList bettingList={eventList} />
-          </section>
       </div>
   );
 }
