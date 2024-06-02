d0785664d97f0ca444c83e1c66516ab4431ffa77
이재현 <2023050@bssm.hs.kr>
2024-06-03 00:27:40 +0900

    FIX :: App.jsx 재수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import './App.css';
 import './fonts.css'
 import './reset.css'
 import Sidebar from "./components/sidebar/sidebar";
-import BettingList from './pages/betting/list/BettingList';
 
 function App() {
-  return (
-      <div className="App">
-          <Sidebar />
-      </div>
-  );
+    return (
+        <div className="App">
+            <Sidebar />
+        </div>
+    );
 }
 
 export default App;
