febed14b0a78aa7a319bb91fd042b64d17c7eb21
kangsiwoo <2023003@bssm.hs.kr>
2024-06-03 00:16:26 +0900

    ADD :: Menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import './App.css';
 import './fonts.css'
 import './reset.css'
 import Sidebar from "./components/sidebar/sidebar";
+import Home from "./components/home/home.jsx";
+
+import BettingDetail from "./components/betting/BettingDetail.jsx"
 
 function App() {
     return (
         <div className="App">
             <Sidebar />
+            <Home />
+
+            {/*<BettingDetail />*/}
         </div>
     );
 }
