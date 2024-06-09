da9e935dac2c754c5be3d6dd001c0fd7b7e87328
kangsiwoo <2023003@bssm.hs.kr>
2024-06-10 01:37:57 +0900

    ADD :: insert home to App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import './fonts.css'
 import './reset.css'
 import Sidebar from "../../BTO_Clinet/src/components/sidebar/sidebar.jsx";
+import Home from "./components/Home/Home.jsx";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <Home />
     </div>
   );
 }
